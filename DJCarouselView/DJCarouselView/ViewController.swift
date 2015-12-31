3484fc4b0d94b7664e9e170131aec93032d5334f
SuperDJ <SuperDJ@bitbucket.org>
2015-12-31 19:29:09 +0800

    init project

---
+++ DJCarouselView/DJCarouselView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DJCarouselView
+//
+//  Created by SuperDJ on 15/12/31.
+//  Copyright © 2015年 SuperDJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
